f399097f5a7e7cd652c61524482753db1c056c96
Cong Do <dochauthanhcong147@gmail.com>
2022-03-27 02:29:40 +0700

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import Joi from 'joi';
+
+const envVarsSchema = Joi.object()
+    .keys({
+        NODE_ENV: Joi.string()
+            .valid('production', 'development', 'test')
+            .required(),
+        PORT: Joi.number().default(3000),
+    })
+    .unknown();
+
+const { value: envVars, error } = envVarsSchema
+    .prefs({ errors: { label: 'key' } })
+    .validate(process.env);
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+export default {
+    env: envVars.NODE_ENV,
+    port: envVars.PORT,
+};
